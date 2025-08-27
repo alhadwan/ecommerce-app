e8cd68619d6904aefc1e04100200553440e8228d
alhadwan <ali.hadwan@ymail.com>
2025-08-26 21:11:44 -0700

    fix: image path

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -4,9 +4,13 @@ import React from "react";
 const ProductItem = ({ productItem }) => {
   return (
     <div className="card">
-      <img
+      {/* <img
         className="card-image"
         src={productItem.image}
+        alt={productItem.name}
+      /> */}
+      <img
+        src={`${import.meta.env.BASE_URL}${productItem.image.replace(/^\//, "")}`}
         alt={productItem.name}
       />
       <div className="card-content">
